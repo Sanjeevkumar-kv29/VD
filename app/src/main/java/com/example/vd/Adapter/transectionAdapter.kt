04bafc390163fa23e0564b6d22d29e255dca6f9f
sanjeev kumar suryawanshi <you@example.com>
2020-11-18 13:47:09 +0530

    submain2

---
+++ app/src/main/java/com/example/vd/Adapter/transectionAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.Adapter
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.vd.R
+
+class CategoryListAdapter(private val context: Activity, private val amount: ArrayList<String>, private val trnsctndate: ArrayList<String>)
+    : ArrayAdapter<String>(context, R.layout.cstmtransectionlist, amount) {
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.cstmtransectionlist, null, true)
+
+        val amountText = rowView.findViewById(R.id.amount) as TextView
+        val trnsctndateText = rowView.findViewById(R.id.trnsctnDate) as TextView
+
+
+        amountText.text = amount[position]
+        trnsctndateText.text = trnsctndate[position]
+        return rowView
+    }
+}
